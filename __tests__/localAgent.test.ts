3e53069fb2fe4a6cd2fb69d5506e793f05dfd5f1
Martin Riedel <web@riedel-it.de>
2021-08-04 00:09:36 -0400

    feat: moved all suite-related code into VeramoLDSignature implementations

--- __tests__/localAgent.test.ts
+++ __tests__/localAgent.test.ts
@@ -20,6 +20,9 @@ import {
   LdCredentialModule,
   LdContextLoader,
   LdDefaultContexts,
+  LdSuiteLoader,
+  VeramoEd25519Signature2018,
+  VeramoEcdsaSecp256k1RecoverySignature2020
 } from '../packages/credential-w3c/src'
 import { EthrDIDProvider } from '../packages/did-provider-ethr/src'
 import { WebDIDProvider } from '../packages/did-provider-web/src'
@@ -191,6 +194,12 @@ const setup = async (options?: IAgentOptions): Promise<boolean> => {
               contextsPaths: [
                 LdDefaultContexts,
                 credential_contexts as Map<string, object>
+              ]
+            }),
+            ldSuiteLoader: new LdSuiteLoader({
+              veramoLdSignatures: [
+                new VeramoEd25519Signature2018(),
+                new VeramoEcdsaSecp256k1RecoverySignature2020()
               ]
             })
           })
